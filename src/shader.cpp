22638b0ca0ff01f1c04916f24507f578c09277c4
Robert O'Shea <robertoshea2k11@gmail.com>
2023-12-08 01:34:03 +0000

    Refactor shader code and add ShaderBuilder class

--- src/shader.cpp
+++ src/shader.cpp
@@ -1,106 +1,11 @@
 #include "shader.hpp"
 
-#include <fstream>
-#include <sstream>
-#include <iostream>
+Shader::Shader() : m_ID(-1) {}
+Shader::Shader(unsigned int programID) : m_ID(programID) {}
 
-static inline int checkShaderCompileStatus(unsigned int shader) {
-  int success;
-  char infoLog[512];
-  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(shader, 512, nullptr, infoLog);
-    std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
-  }
-  return success;
-}
+unsigned int Shader::GetID() const { return m_ID; }
 
-static inline int checkShaderLinkStatus(unsigned int program) {
-  int success;
-  char infoLog[512];
-  glGetProgramiv(program, GL_LINK_STATUS, &success);
-  if (!success) {
-    glGetProgramInfoLog(program, 512, nullptr, infoLog);
-    std::cout << "ERROR::SHADER::LINK_FAILED\n" << infoLog << std::endl;
-  }
-  return success;
-}
-
-Shader::Shader(const char *vertexPath, const char *fragmentPath) : m_ID(0), m_Valid(true) {
-  std::string vertexCode;
-  std::string fragmentCode;
-  std::ifstream vShaderFile;
-  std::ifstream fShaderFile;
-
-  vShaderFile.exceptions(std::ifstream::failbit || std::ifstream::badbit);
-  fShaderFile.exceptions(std::ifstream::failbit || std::ifstream::badbit);
-
-  try {
-    vShaderFile.open(vertexPath);
-    fShaderFile.open(fragmentPath);
-    std::stringstream vShaderStream, fShaderStream;
-    vShaderStream << vShaderFile.rdbuf();
-    fShaderStream << fShaderFile.rdbuf();
-    vShaderFile.close();
-    fShaderFile.close();
-    vertexCode = vShaderStream.str();
-    fragmentCode = fShaderStream.str();
-  } catch (std::ifstream::failure e) {
-    std::cout << "ERROR:SHADER::FILE_NOT_SUCCESSFULL_READ" << std::endl;
-  }
-
-  const char *vCode = vertexCode.c_str();
-  const char *fCode = fragmentCode.c_str();
-
-  unsigned int vertex, fragment;
-  vertex = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertex, 1, &vCode, nullptr);
-  glCompileShader(vertex);
-  if (!checkShaderCompileStatus(vertex)) {
-    m_Valid = false;
-    goto vert_error;
-  }
-
-  fragment = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragment, 1, &fCode, nullptr);
-  glCompileShader(fragment);
-  if (!checkShaderCompileStatus(fragment)) {
-    m_Valid = false;
-    goto frag_error;
-  }
-
-  m_ID = glCreateProgram();
-  glAttachShader(m_ID, vertex);
-  glAttachShader(m_ID, fragment);
-  glLinkProgram(m_ID);
-  if (!checkShaderLinkStatus(m_ID)) {
-    m_Valid = false;
-    goto link_error;
-  }
-
-  return;
-
-link_error:
-  glDeleteProgram(m_ID);
-
-frag_error:
-  glDeleteShader(fragment);
-
-vert_error:
-  glDeleteShader(vertex);
-}
-
-unsigned int Shader::GetID() const {
-  return m_ID;
-}
-
-bool Shader::IsValid() const {
-  return m_Valid;
-}
-
-void Shader::Use() const {
-  glUseProgram(m_ID);
-}
+void Shader::Use() const { glUseProgram(m_ID); }
 
 void Shader::SetBool(const std::string &name, bool value) const {
   glUniform1i(glGetUniformLocation(m_ID, name.c_str()), (int)value);
@@ -121,9 +26,11 @@ void Shader::SetFloat(unsigned int location, float value) const {
   glUniform1f(location, value);
 }
 
-void Shader::SetVec4f(const std::string &name, float x, float y, float z, float w) const {
+void Shader::SetVec4f(const std::string &name, float x, float y, float z,
+                      float w) const {
   glUniform4f(glGetUniformLocation(m_ID, name.c_str()), x, y, z, w);
 }
-void Shader::SetVec4f(unsigned int location, float x, float y, float z, float w) const {
+void Shader::SetVec4f(unsigned int location, float x, float y, float z,
+                      float w) const {
   glUniform4f(location, x, y, z, w);
 }
---
+++ src/shaderbuilder.cpp
@@ -0,0 +1,80 @@
+#include "shader.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+static inline int checkShaderCompileStatus(unsigned int shader) {
+  int success;
+  char infoLog[512];
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+    std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+  return success;
+}
+
+static inline int checkShaderLinkStatus(unsigned int program) {
+  int success;
+  char infoLog[512];
+  glGetProgramiv(program, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(program, 512, nullptr, infoLog);
+    std::cout << "ERROR::SHADER::LINK_FAILED\n" << infoLog << std::endl;
+  }
+  return success;
+}
+
+ShaderBuilder::ShaderBuilder() : m_ShaderEntries() {}
+
+ShaderBuilder &ShaderBuilder::AddShader(GLenum type, const char *path) {
+  m_ShaderEntries.push_back({type, std::string(path)});
+  return *this;
+}
+ShaderBuilder &ShaderBuilder::AddShader(GLenum type, std::string &path) {
+  m_ShaderEntries.push_back({type, path});
+  return *this;
+}
+
+std::optional<Shader> ShaderBuilder::Build() {
+  std::vector<unsigned int> shaders;
+  std::string shaderCode;
+  std::ifstream file;
+  file.exceptions(std::ifstream::failbit || std::ifstream::badbit);
+  for (const ShaderEntry &entry : m_ShaderEntries) {
+    try {
+      std::stringstream stream;
+      file.open(entry.path);
+      stream << file.rdbuf();
+      file.close();
+      shaderCode = stream.str();
+    } catch (std::ifstream::failure e) {
+      std::cout << "ERROR:SHADER::FILE_NOT_SUCCESSFULL_READ" << std::endl;
+      return std::nullopt;
+    }
+    const char *code = shaderCode.c_str();
+    unsigned int shader = glCreateShader(entry.type);
+    glShaderSource(shader, 1, &code, nullptr);
+    glCompileShader(shader);
+    if (!checkShaderCompileStatus(shader)) {
+      return std::nullopt;
+    }
+    shaders.push_back(shader);
+  }
+
+  unsigned int id = glCreateProgram();
+  for (unsigned int shader : shaders) {
+    glAttachShader(id, shader);
+  }
+  glLinkProgram(id);
+  if (!checkShaderLinkStatus(id)) {
+    return std::nullopt;
+  }
+
+  for (unsigned int shader : shaders) {
+    glDeleteShader(shader);
+  }
+
+  return std::optional<Shader>(Shader(id));
+}
