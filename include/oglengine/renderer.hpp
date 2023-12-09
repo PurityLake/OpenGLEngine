45a29fd5282949f6e5db7f6e7c8d9cff154a785c
Robert O'Shea <robertoshea2k11@gmail.com>
2023-12-09 20:56:05 +0000

    Added Renderer class

--- include/oglengine/renderer.hpp
+++ include/oglengine/renderer.hpp
@@ -3,6 +3,7 @@
 
 #include <oglengine/model.hpp>
 
+#include <iostream>
 #include <vector>
 
 class Renderer {
@@ -11,48 +12,7 @@ public:
   ~Renderer() = default;
 
   void Draw(const std::vector<Model> &models, glm::mat4 &view,
-            glm::mat4 &projection) const {
-
-    auto modelMat =
-        glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f)),
-                   glm::vec3(1.0f, 1.0f, 1.0f));
-    for (const auto &model : models) {
-      for (const auto &mesh : model.m_Meshes) {
-        model.shader.Use();
-        model.shader.SetMat4("model", modelMat);
-        model.shader.SetMat4("view", view);
-        model.shader.SetMat4("projection", projection);
-
-        unsigned int diffuseNr = 1;
-        unsigned int specularNr = 1;
-        unsigned int normalNr = 1;
-        unsigned int heightNr = 1;
-
-        for (int i = 0; i < mesh.m_Textures.size(); ++i) {
-          glActiveTexture(GL_TEXTURE0 + i);
-          unsigned int id = mesh.m_Textures[i].ID;
-          std::string number;
-          std::string name;
-          glBindTexture(GL_TEXTURE_2D, id);
-          if (name == "texture_diffuse") {
-            number = std::to_string(diffuseNr++);
-          } else if (name == "texture_specular") {
-            number = std::to_string(specularNr++);
-          } else if (name == "texture_normal") {
-            number = std::to_string(normalNr++);
-          } else if (name == "texture_height") {
-            number = std::to_string(heightNr++);
-          }
-          model.shader.SetInt((name + number).c_str(), i);
-        }
-        glBindVertexArray(mesh.m_VAO);
-        glDrawElements(GL_TRIANGLES, mesh.m_Indices.size(), GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
-        glActiveTexture(GL_TEXTURE0);
-      }
-    }
-  }
+            glm::mat4 &projection) const;
 };
 
 #endif // __HPP_RENDER__
---
+++ src/renderer.cpp
@@ -0,0 +1,44 @@
+#include "oglengine/renderer.hpp"
+
+void Renderer::Draw(const std::vector<Model> &models, glm::mat4 &view,
+                    glm::mat4 &projection) const {
+  glm::mat4 matModel = glm::mat4(1.0f);
+  matModel = glm::translate(matModel, glm::vec3(0.0f, 0.0f, 0.0f));
+  matModel = glm::scale(matModel, glm::vec3(1.50, 1.0f, 1.0f));
+  for (const auto &model : models) {
+    for (const auto &mesh : model.m_Meshes) {
+      model.m_Shader.Use();
+      model.m_Shader.SetMat4("model", matModel);
+      model.m_Shader.SetMat4("view", view);
+      model.m_Shader.SetMat4("projection", projection);
+
+      unsigned int diffuseNr = 1;
+      unsigned int specularNr = 1;
+      unsigned int normalNr = 1;
+      unsigned int heightNr = 1;
+
+      for (int i = 0; i < mesh.m_Textures.size(); ++i) {
+        glActiveTexture(GL_TEXTURE0 + i);
+        unsigned int id = mesh.m_Textures[i].ID;
+        std::string number;
+        std::string name = mesh.m_Textures[i].Type;
+        glBindTexture(GL_TEXTURE_2D, id);
+        if (name == "texture_diffuse") {
+          number = std::to_string(diffuseNr++);
+        } else if (name == "texture_specular") {
+          number = std::to_string(specularNr++);
+        } else if (name == "texture_normal") {
+          number = std::to_string(normalNr++);
+        } else if (name == "texture_height") {
+          number = std::to_string(heightNr++);
+        }
+        model.m_Shader.SetInt((name + number).c_str(), i);
+      }
+      glBindVertexArray(mesh.m_VAO);
+      glDrawElements(GL_TRIANGLES, mesh.m_Indices.size(), GL_UNSIGNED_INT, 0);
+      glBindVertexArray(0);
+
+      glActiveTexture(GL_TEXTURE0);
+    }
+  }
+}
